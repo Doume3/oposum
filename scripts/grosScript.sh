1a4d3d17f0427f1fd8c517d365e33738cbdd5b07
Ilu <j.bressand.34@gmail.com>
2016-03-18 17:51:03 +0100

    ajout grosScript

---
+++ scripts/grosScript.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+ERR_ARGS=85
+
+if [ $# -ne 3 ]  # Correct number of arguments passed to script?
+then
+  echo "Usage: `basename $0` nom_app taille_vm nb_vm"
+  exit $ERR_ARGS
+fi
+
+case $1 in
+    chat|FTP) 
+	echo "Application : $1";;
+    *)
+	echo "Usage chat | FTP : $1";
+	exit $ERR_ARGS;;
+esac
+
+case $2 in
+    xs|tiny|small|medium|large|xlarge)
+        echo "Flavor : $2";;
+    *)
+	echo "Usage: xs | tiny | small | medium | large | xlarge : $2";
+	exit $ERR_ARGS;;
+esac
+
+if [ $3 -gt 10 ]
+then
+	echo "Usage : nombre de vm < 10 : $3";
+	exit $ERR_ARGS;
+fi
+
+
+echo '#+------------------------+';
+echo '#|       VM_LAUNCHER      |';
+echo '#+------------------------+';
+
+for i in `seq 1 $3`;
+do
+	rake cmd cmd="echo '#### START_RAKE ####';
+	source openstack-openrc.sh;
+
+	echo '#### CREATION VM1 ####';
+	nova boot --flavor m1.$2 --image 'Debian Jessie 64-bit' --nic net-id=\$(neutron net-show -c id -f value private) --key_name demo default$i;
+
+
+	echo '#### AJOUTE IP PUBLIQUE ####';
+	IP_PUB=\`nova floating-ip-create public | grep -o -E '(([0-9]{1,3}\.){3}[0-9]{1,3})'\`;
+	nova add-floating-ip default$i \$IP_PUB;
+	echo \$IP_PUB;
+
+	echo '#### MODIFIE DROITS ####';
+	VAR_RULE=\`(nova secgroup-list-rules default | grep -o 10000)\`;
+
+	if [ -z \$VAR_RULE ]
+	then
+		nova secgroup-add-rule default tcp 10000 10100 0.0.0.0/0;
+	fi
+	 " host=controller;
+	
+done
+
+#On fetch l'adresse du controller
+ADR=`rake roles:show | grep 'controller' | grep -o -E '[^: ]*\.grid5000\.fr'`;
+echo "*Adresse du controller* > $ADR";
+
+#On s'y connecte pour fetch la liste des IP_VM
+IPs=`ssh root@$ADR 'source openstack-openrc.sh && nova floating-ip-list' | cut -d '|' -f 3 | grep -o -E '(([0-9]{1,3}\.){3}[0-9]{1,3})'`;
+echo "*IP VMs* > $IPs";
+
+for IP in $IPs; do
+echo "VM : $IP";
+
+scp -p -r ../../Serveur/ debian@$IP: ; #/!\ les applications sont présentes sur le frontend
+ssh debian@$IP "sudo apt-get -y update; sudo apt-get -y install gcc make; cd Serveur/$1/; make;"; # Serveur/chat ou Serveur/FTP
+
+echo 'Apps installees';
+
+done
+
+
