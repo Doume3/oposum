bbeba6fbcb7b5f6725765cc6fa6f6de4a2d79df3
charles <benais.c@gmail.com>
2016-03-23 17:48:34 +0100

    Nettoyage

--- scripts/connection.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ssh root@edel-27.grenoble.grid5000.fr 'source openstack-openrc.sh;./ipfloat.sh' > ipfloat.txt
-read nomscript
-ssh debian@10.132.4.11 bash < $nomscript
-ssh debian@10.132.4.12 bash < $nomscript
--- scripts/ipfloat.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-nova floating-ip-list | egrep -e "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\ " | cut -d '|' -f 3 | cut -c 2-
