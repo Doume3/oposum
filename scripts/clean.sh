22916161f9c86901ce7189c22175da6975d6d6f0
Ilu <j.bressand.34@gmail.com>
2016-03-23 18:42:00 +0100

    modif clean.sh charles

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -10,10 +10,8 @@ rake cmd cmd="source openstack-openrc.sh;
 VMVAR=\`nova list | cut -d '|' -f 3 | cut -c 2- | grep -v -E 'Name|--+'\`;
 for VM in \$VMVAR;do
 nova delete \$VM;
-done" host=controller;
-
-rake cmd cmd="source openstack-openrc.sh;
-
+done;
+\`sleep 2;\`
 IPPUB=\`nova floating-ip-list | cut -d '|' -f 3 | grep -o -E '(([0-9]{1,3}\.){3}[0-9]{1,3})'\`;
 for IP in \$IPPUB; do
 nova floating-ip-delete \$IP;
