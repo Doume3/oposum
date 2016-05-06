dc3ce3fb278bd3f234443b60089f602e12eb7775
alexdu98 <alexdu98@gmx.fr>
2016-05-07 01:21:44 +0200

    Modif pour v2

--- scripts/appSetup.sh
+++ scripts/appSetup.sh
@@ -2,27 +2,18 @@
 
 ERR_ARGS=-1
 
-if [ $# -ne 5 ] && [ $# -ne 6 ]; then
-  echo "Usage: `basename $0` nom_VM nom_APP type_APP path_APP port_APP [type_APP=client=>serveur_APP]"
+if [ $# -ne 4 ]; then
+  echo "Usage: `basename $0` nom_VM nom_APP type_APP param_APP"
   exit $ERR_ARGS
 fi
 
-echo "- Vérification du type de l'application et des parametres";
+echo "- Vérification des parametres";
 case $3 in
     client)
-	if [ $# -ne 6 ]; then
-		echo "Usage: `basename $0` nom_VM nom_APP type_APP path_APP port_APP serveur_APP"
-  		exit $ERR_ARGS
-	fi
 	;;
     normal)
 	;;
-    serveur)
-	echo "- Vérification du port de l'application";
-	if [ $5 -lt 10000 ] && [ $5 -gt 10100 ]; then
-       		echo "port_APP: 10000 à 10100 : $5"
-        	exit $ERR_ARGS
-	fi	
+    serveur)	
 	;;	
     *)
 	echo "type_APP: client | serveur | normal : $3"
@@ -38,24 +29,10 @@ ADR=`rake roles:show | grep 'controller' | grep -o -E '[^: ]*\.grid5000\.fr'`;
 # On se connecte au controleur pour récupérer l'IP de la VM
 IP=`ssh root@$ADR "source openstack-openrc.sh && nova list --name $1" | cut -d '|' -f 7 | grep -o -E '(10\.([0-9]{1,3}\.){2}[0-9]{1,3})'`;
 
-if [ "$3" = "client" ]; then
-	# On se connecte au controleur pour récupérer l'IP de la VM serveur
-	IPServeur=`ssh root@$ADR "source openstack-openrc.sh && nova list --name $6" | cut -d '|' -f 7 | grep -o -E '(10\.([0-9]{1,3}\.){2}[0-9]{1,3})'`;
-	echo "IP VM serveur : $6:$IPServeur"
-	if [ -z "$IPServeur" ]; then
-	        echo "L'IP de la VM serveur est introuvable"
-	        exit $ERR_ARGS
-	fi
-fi
-
 echo "- Copie de l'application ($2) sur la VM"
-scp -q -p -r $4 debian@$IP: >> $LOG
+scp -q -p -r ../apps/$2 debian@$IP: >> $LOG
 
 echo "- Démarrage de l'application"
-if [ "$3" = "client" ]; then
-	ssh -q debian@$IP "cd $2; make $3; ls -l; ./$3 $IPServeur $5" >> $LOG &
-else
- 	ssh -q debian@$IP "cd $2; make $3; ls -l; ./$3 $5" >> $LOG &
-fi
+ssh -q debian@$IP "cd $2; make $3; ls -l; ./$3 $4" >> $LOG &
 
 exit 0
--- scripts/construct.rb
+++ scripts/construct.rb
@@ -1,55 +1,41 @@
 require 'rubygems'
 require 'json'
 
-file = File.read('config.json') # close le file ?
+file = File.read('config.json')
 data = JSON.parse(file)
 
-def getPortServeur(data, nomVM, nomAPP)
-	data["vms"].each do |vm|
-		if vm["nom"] == nomVM
-			vm["apps"].each do |app|
-				if app["nom"] == nomAPP and app["type"] == "serveur"
-					return app["port"]
-				end
-			end
-		end
-	end
-	return false
-end
-
-data["vms"].each do |vm|
+data["apps"].each do |app|
 	print "\n"
-	for numVM in 1..vm["nombreCopie"]
-		puts "### Création de la machine virtuelle '#{vm["nom"]}#{numVM}'"
-		resVM = system("./VMSetup.sh \"#{vm["type"]}\" \"#{vm["nom"]}#{numVM}\"")
+	for numVM in 1..app["nbVMs"]
+		puts "### Création de la machine virtuelle '#{app["nomVMs"]}#{numVM}'"
+		resVM = system("./VMSetup.sh \"#{app["typeVMs"]}\" \"#{app["nomVMs"]}#{numVM}\"")
 		if resVM != false
-			puts "### Machine virtuelle créée avec succès (#{vm["nom"]}#{numVM})"
-			vm["apps"].each do |app|
-				print "\n"
-				if app["publique"] == true
-					rep = "pub" 
-				else
-					rep = "pers"
-				end
-				if File.directory?("../apps/#{rep}/#{app["nom"]}") == true
-					puts "### Installation de l'application '#{app["nom"]}'"
-					if app["type"] == "serveur"
-		         			resApp = system("./appSetup.sh \"#{vm["nom"]}#{numVM}\" \"#{app["nom"]}\" \"#{app["type"]}\" \"../apps/#{rep}/#{app["nom"]}\" \"#{app["port"]}\"")
-        				else
-						portServeur = getPortServeur(data, app["serveur"], app["nom"])
-						resApp = system("./appSetup.sh \"#{vm["nom"]}#{numVM}\" \"#{app["nom"]}\" \"#{app["type"]}\" \"../apps/#{rep}/#{app["nom"]}\" \"#{app["portServeur"]}\" \"#{app["serveur"]}\"")
+			puts "### Machine virtuelle créée avec succès (#{app["nomVMs"]}#{numVM})"
+			puts "### Installation de l'application '#{app["nomApp"]}'"
+			if app["typeApp"] == "client"
+				tabParam = app["parametresApp"].split(/ /)
+				tabParam.each do |param|
+					if nomVM = param.match(/{{IPVM:(.+)}}/)
+						puts "NOMVM : #{nomVM[1]}"
+						adr = `rake roles:show | grep 'controller' | grep -o -E '[^: ]*\.grid5000\.fr'`
+						puts "ADR : #{adr}"
+						cmd = "ssh root@#{adr} 'source openstack-openrc.sh && nova list --name #{nomVM[1]}1' | grep -o -E '(10\.([0-9]{1,3}\.){2}[0-9]{1,3})'"
+						puts "CMD : #{cmd}"
+						ip = `#{cmd}`
+						puts "IP : #{ip}"
+						app["parametresApp"] = app["parametresApp"].gsub(/{{IPVM:.+}}/, ip)
+						puts "PARAM : #{app["parametresApp"]}"
 					end
-					if resApp != false
-        	      	            		puts "### Application installée avec succès (#{app["nom"]})"
-        	   	                else
-        	                        	puts "### Une erreur est survenue, l'application (#{app["nom"]}) n'a pas été installée"
-        	                    	end
-				else
-					puts "### Application introuvable"
 				end
 			end
+			resApp = system("./appSetup.sh \"#{app["nomVMs"]}#{numVM}\" \"#{app["nomApp"]}\" \"#{app["typeApp"]}\" \"#{app["parametresApp"]}\"")
+			if resApp != false
+        	      		puts "### Application installée avec succès (#{app["nomApp"]})"
+        	   	else
+        	               	puts "### Une erreur est survenue, l'application (#{app["nomApp"]}) n'a pas été installée"
+        	        end
 		else
-			puts "### Une erreur est survenue, la machine virtuelle (#{vm["nom"]}#{numVM}) n'a pas été créée"
+			puts "### Une erreur est survenue, la machine virtuelle (#{app["nomVMs"]}#{numVM}) n'a pas été créée"
 		end
 		print "\n\n"
 	end
